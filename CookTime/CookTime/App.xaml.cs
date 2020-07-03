4fd94eed251dc85b5594b45c8f553017581028a8
MAU <mau141226@gmail.com>
2020-07-02 22:12:35 -0600

    [Add] Automatic files

---
+++ CookTime/CookTime/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CookTime.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookTime
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new NavigationPage(new Views.LoginPage());
+
+
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CookTime/CookTime/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CookTime
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int cont = 0;
+        protected override void OnAppearing()
+        {
+            Sumarbtn.Clicked += Sumarbtn_Clicked;
+        }
+
+        private void Sumarbtn_Clicked (object sender ,  EventArgs e)
+        {
+            cont++;
+            Sumarbtn.Text = cont.ToString();
+        }
+    }
+}
