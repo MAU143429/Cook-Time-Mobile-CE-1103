e1d925a83a49b77a674eb4fef84cfb4b7162cb95
MAU <mau141226@gmail.com>
2020-07-08 00:45:55 -0600

    [Add] Added a new pages to search recipes

---
+++ CookTime/CookTime/Views/ShowSearch.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookTime.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowSearch : ContentPage
+    {
+        public ShowSearch()
+        {
+            InitializeComponent();
+        }
+        private void View_Recipe(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ViewRecipe());
+
+        }
+    }
+}
