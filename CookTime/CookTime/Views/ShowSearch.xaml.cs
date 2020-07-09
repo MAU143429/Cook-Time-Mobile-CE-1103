3a4a4207047a53f12ce238b1de8639a9c905d8d5
Jose Antonio Espinoza <joseantonioestudiante2001@gmail.com>
2020-07-09 00:45:52 -0600

    [Add] All user search method
    Created the main logic for bringing up all the users registered, still doesn´t work because of RegisterModel json sintaxis.

--- CookTime/CookTime/Views/ShowSearch.xaml.cs
+++ CookTime/CookTime/Views/ShowSearch.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using CookTime.REST_API_Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace CookTime.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,18 @@ namespace CookTime.Views
         public ShowSearch()
         {
             InitializeComponent();
+            Pull_Search_Request();
+        }
+        private async void Pull_Search_Request()
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://192.168.0.17:6969/user";
+            var result = await client.GetAsync(url);
+            var json = result.Content.ReadAsStringAsync().Result;
+            RegistrationModel model = RegistrationModel.FromJson(json);
+            Console.WriteLine(json);
+            ListaUsers.ItemsSource = model.Newusers;
+            
         }
         private void View_Recipe(object sender, EventArgs e)
         {
