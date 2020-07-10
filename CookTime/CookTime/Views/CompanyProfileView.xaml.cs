73ca2dd1884c5f13bf19c8c97e6a9bc0a9dd51ad
MAU <mau141226@gmail.com>
2020-07-10 00:13:59 -0600

    [Add] Added new view pages for companies, recipes and users

---
+++ CookTime/CookTime/Views/CompanyProfileView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookTime.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CompanyProfileView : ContentPage
+    {
+        public CompanyProfileView()
+        {
+            InitializeComponent();
+        }
+        private void View_List(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CompanyMemberList());
+
+        }
+    }
+}
