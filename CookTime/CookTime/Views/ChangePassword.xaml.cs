e7736358d16284d5e82be892cabe14c2d6e66933
MAU <mau141226@gmail.com>
2020-07-15 20:30:55 -0600

    [Add] Added new pages to change the password and profile photo
    New pages in Views Package

---
+++ CookTime/CookTime/Views/ChangePassword.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookTime.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChangePassword : ContentPage
+    {
+        public ChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new HomePage());
+
+        }
+    }
+}
