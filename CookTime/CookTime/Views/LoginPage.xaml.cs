a51248b0fc747b806358ee9a40217f6dd6cb92ec
Jose Antonio Espinoza <joseantonioestudiante2001@gmail.com>
2020-07-16 00:08:32 -0600

    [Update] Changed the login logical method

--- CookTime/CookTime/Views/LoginPage.xaml.cs
+++ CookTime/CookTime/Views/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using CookTime.REST_API_LoginModel;
+using CookTime.REST_API_UserModel;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,7 @@ namespace CookTime.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        CookTime.REST_API_UserModel.User CURRENTUSER;
         public LoginPage()
         {
             InitializeComponent();
@@ -30,29 +33,46 @@ namespace CookTime.Views
             var userValidate = userEntry.Text;
             if (!string.IsNullOrEmpty(userValidate))
             {
-                LoginUser user = new LoginUser();
-                user.Email = userEntry.Text;
-                user.Password = CreateMD5(pasEntry.Text);
-                HttpClient cliente = new HttpClient();
-                string url = "http://192.168.0.17:6969/test";//LOGIN VERIFICATION
-                String jsonNewUser = JsonConvert.SerializeObject(user);
-                Console.WriteLine("JSON NEW USER:" + jsonNewUser);
-                var datasent = new StringContent(jsonNewUser);
-                Console.WriteLine("DATASENT" + datasent);
-                datasent.Headers.ContentType.MediaType = "application/json";
-                var result = await cliente.PostAsync(url, datasent);
                 
+                string email = userEntry.Text;
+                string password = "/"+CreateMD5(pasEntry.Text);
+                HttpClient cliente = new HttpClient();
+                string url = "http://192.168.0.17:6969/login/" + email + password;
+                //String jsonNewUser = JsonConvert.SerializeObject(user);
+               // Console.WriteLine("JSON NEW USER:" + jsonNewUser);
+                //var datasent = new StringContent(jsonNewUser);
+                //Console.WriteLine("DATASENT" + datasent);
+                //datasent.Headers.ContentType.MediaType = "application/json";
+                var result = await cliente.GetAsync(url);
                 var json = result.Content.ReadAsStringAsync().Result;
-                /*
-                 * if json != false:
-                 * alerta!
-                 * else{
-                 * user = new USer() con los datos que me manda
-                 * }
-                 * 
-                 */
-                await DisplayAlert("Result", json, "ok");
-                await Navigation.PushAsync(new HomePage());
+
+                var nullUser = new REST_API_UserModel.User();
+                nullUser.Password = null;
+                nullUser.Followers = null;
+                nullUser.Following = null;
+                nullUser.Name = null;
+                nullUser.Recipes = null;
+                nullUser.Hascompany = false;
+                nullUser.Email = null;
+                nullUser.Age = 0;
+
+                string NullCompareable = JsonConvert.SerializeObject(nullUser);
+                
+
+
+                if (json == NullCompareable)
+                {
+                   await  DisplayAlert("Cook Time","The data you filled with does not match with any CookTime user. Please verify your info and try again!","OK") ;
+                }
+                else
+                {
+                    CURRENTUSER = CookTime.REST_API_UserModel.User.FromJson(json);
+                    Console.WriteLine(CURRENTUSER.Name);
+                    await Navigation.PushAsync(new HomePage());
+                }
+                 
+                
+                
             }
             
         }
